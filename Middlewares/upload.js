83e3836b723a2d29e080201f78effa783dc71658
promise <emmanuelpromise456@gmail.com>
2024-08-22 17:58:03 +0100

    Updated the upload.js file

--- Middlewares/upload.js
+++ Middlewares/upload.js
@@ -12,17 +12,20 @@ const storage = multer.diskStorage({
 // Initialize upload
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 50000000 }, // Limit file size to 10MB
+  limits: { fileSize: 50000000 }, // 50MB
   fileFilter: function (req, file, cb) {
     checkFileType(file, cb);
   }
 });
 
-// Check file type
 function checkFileType(file, cb) {
-  const filetypes = /jpeg|jpg|png|jfif|gif|mp4|mp3/;
+  const filetypes = /jpeg|jpg|png|jfif|gif|mp4|mp3|wav|flac|ogg/;
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
   const mimetype = filetypes.test(file.mimetype);
+
+  console.log('File Original Name:', file.originalname);
+  console.log('File Extension:', path.extname(file.originalname).toLowerCase());
+  console.log('File MIME Type:', file.mimetype);
 
   if (mimetype && extname) {
     return cb(null, true);
